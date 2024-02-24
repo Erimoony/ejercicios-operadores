2533c107017ea79c66fc127042a13c8d0b1e0719
Luna <lunanino2705@gmail.com>
2024-02-24 16:55:47 -0500

    2. Ingresar dos numeros por teclado y sumarlos

--- src/main/java/co/edu/sena/opp2902274/App.java
+++ src/main/java/co/edu/sena/opp2902274/App.java
@@ -24,6 +24,25 @@ public class App
         //salida
         System.out.println( "El área del triangulo es:" + result);
 
+         //Suma de dos número
+        // entrada
+        System.out.println("2. Ingresar dos numeros por teclado y sumarlos");
+        System.out.println("Ingrese el primer número: ");
+
+
+        Scanner sum= new Scanner(System.in);
+        int n1 =src.nextInt();
+        
+        System.out.println("Ingrese el segundo número");
+        int n2 = src.nextInt();
+
+
+        //proceso 
+        int suma =(n1 + n2);
+
+
+        //salida
+        System.out.println( "La suma de estos numeros es: " + suma);
 
 
 
